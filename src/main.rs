3b0fd61117d7e2972823780c155ebb23f7b6a692
Evgeny Beskonchin <evgeny@beskonchin.info>
2021-03-07 13:34:06 +0700

    threads count show

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use std::time::Instant;
 fn main() {
     let now = Instant::now();
 
+    println!("Threads count: {}", rayon::current_num_threads());
+
     rayon::scope(|spawner| {
         (0..REPEAT_TIMES).for_each(|_| {
             spawner.spawn(move |_| {

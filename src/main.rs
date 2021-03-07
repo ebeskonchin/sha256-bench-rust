b8c3e9d27cfab9de4cc826d2e4bfb3687eb173e6
Evgeny Beskonchin <evgeny@beskonchin.info>
2021-03-07 12:31:15 +0700

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+const REPEAT_TIMES: usize = 100_000;
+const DATA_LENGTH: usize = 10_000;
+
+use rand::{thread_rng, RngCore};
+use sha2::{Digest, Sha256};
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let now = Instant::now();
+
+    let _ = crossbeam::scope(|spawner| {
+        (0..REPEAT_TIMES).map(|_| gen_bytes()).for_each(|data| {
+            spawner.spawn(move |_| {
+                gen_sha(data);
+            });
+        });
+    });
+
+    println!("Measured: {}ms.", now.elapsed().as_millis())
+}
+
+fn gen_sha(from: [u8; DATA_LENGTH]) {
+    let mut sha = Sha256::new();
+    sha.update(&from);
+    sha.finalize();
+}
+
+fn gen_bytes() -> [u8; DATA_LENGTH] {
+    let mut result = [0u8; DATA_LENGTH];
+    thread_rng().fill_bytes(&mut result);
+    result
 }
